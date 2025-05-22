bdde971fb6e3515492f3bd1f84bcce76a6836fbe
Tanay Sharma <sharmatanay45@gmail.com>
2025-05-23 03:38:09 +0530

    Add basic UI to streamlit app

---
+++ app/main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="WhatsApp CRM", layout="wide")
+st.title("WhatsApp CRM + Smart Campaign Assistant")
+st.write("Welcome! This tool will help you analyze chats, segment users and generate campaigns using AI.")
